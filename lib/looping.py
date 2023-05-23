6d0be86219736fbdeb7eeed117da0c839e10873a
Jay Lim <jlimoman@gmail.com>
2023-05-22 19:49:21 -0600

    complete python-p3-loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while (count > 0):
+        print(f"{count}")
+        count -= 1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = ([int ** 2 for int in int_list])
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while (num <= 100):
+        if (num % 3 == 0 and num % 5 == 0):
+            print("FizzBuzz")
+            num += 1
+        elif (num % 3 == 0):
+            print("Fizz")
+            num += 1
+        elif (num % 5 == 0):
+            print("Buzz")
+            num += 1
+        else:
+            print(num)
+            num += 1
